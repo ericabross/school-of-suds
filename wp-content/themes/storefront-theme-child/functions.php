b7616a3d335e490a75bba6f58b84b5c21c1a5cd8
thisismynightmare <ericabross@gmail.com>
2019-03-09 17:25:38 -0600

    created child theme for storefront

---
+++ wp-content/themes/storefront-theme-child/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Storefront Theme Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage School of Suds
+* @since School of Suds 2.0
+*/
+// Enqueue scripts and styles
+function storefront_child_scripts(){
+	wp_enqueue_style( 'storefront-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'storefront-style' ));
+}
+add_action( 'wp_enqueue_scripts', 'storefront_child_scripts' );
