77e98172e00c9b9f48383d414679bdd4c53d16b6
thisismynightmare <ericabross@gmail.com>
2019-03-09 18:00:18 -0600

    customized the navigation

--- wp-content/themes/storefront-theme-child/functions.php
+++ wp-content/themes/storefront-theme-child/functions.php
@@ -9,9 +9,20 @@
 * @subpackage School of Suds
 * @since School of Suds 2.0
 */
+
+
 // Enqueue scripts and styles
 function storefront_child_scripts(){
 	wp_enqueue_style( 'storefront-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'storefront-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'storefront_child_scripts' );
+
+add_action( 'storefront_header', 'jk_storefront_header_content', 40 );
+	function jk_storefront_header_content() { ?>
+		<link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Raleway" rel="stylesheet">
+		<?php
+	}
+
+
+
